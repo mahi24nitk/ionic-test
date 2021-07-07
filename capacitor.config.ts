e5dd515519b2579ba49d0448f2b6ec3320598b88
Mahendra <mahendra@fbc.nyc>
2021-07-07 15:19:44 +0530

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'gallery',
+  appName: 'photo-gallery',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
