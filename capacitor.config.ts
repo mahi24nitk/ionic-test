10c3786e7b5849b1ad47226094f849e7e0ed0345
Mahendra <mahendra@fbc.nyc>
2021-07-07 16:31:01 +0530

    changes app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'gallery',
+  appId: 'com.galleryionictest1234.app',
   appName: 'photo-gallery',
   webDir: 'build',
   bundledWebRuntime: false
